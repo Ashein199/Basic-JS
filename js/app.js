ff27011f6e4e97f6c83692b10011f3a8dd1b2b21
Ashein199 <heroglasses155@gmail.com>
2021-12-10 11:42:37 +0630

    variable declaration

--- js/app.js
+++ js/app.js
@@ -2,11 +2,21 @@
 //character , number , boolean , string
 // var , let , const -> use in js
 
-var num1 = 10;
-let num2 = 20;
-const num3= 30; //can't override
+// var num1 = 10;
+// let num2 = 20;
+// const num3= 30; //can't override
 
-num1 = 20;
-num2 = 40;
+// num1 = 20;
+// num2 = 40;
 
-console.log(num1, num2);
+// console.log(num1, num2);
+
+// let value = false;
+// console.log(typeof value)
+
+var a = b = 10;
+console.log(a,b)
+
+//don't in variable declaration
+// language keywords (var,if,for)
+// operators 
