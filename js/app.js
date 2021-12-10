168d680c57b4f82b654bfa672923901edb32b81d
Ashein199 <heroglasses155@gmail.com>
2021-12-10 11:18:58 +0630

    data type

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-//output
-document.write("Hello World");
-console.log("Hi console");
-alert("JS Alert");
+//datatype
+//character , number , boolean , string
+// var , let , const -> use in js
 
-//input
-confirm("are u sure ?");
-prompt("enter name");
+var num1 = 10;
+let num2 = 20;
+const num3= 30; //can't override
+
+num1 = 20;
+num2 = 40;
+
+console.log(num1, num2);
