24704098996b24570f4dc076831744d6aa9a9e41
Ashein199 <heroglasses155@gmail.com>
2021-12-25 10:12:06 +0630

    looping part1

--- js/app.js
+++ js/app.js
@@ -3,31 +3,16 @@
 // condition => if , else , elseif , switch
 // loop => while , do-while , for
 
+// looping
 
-var status = prompt("Enter number from 1 to 3");
-// if ( status == 1){
-//     console.log("Morning");
-// }else if( status==2){
-//     console.log("Afternoon");
-// }else if( status==3){
-//     console.log("Night");
-// }else{
-//     console.log("Error number");
-// }
-// console.log("Have a nice day !");
+var num = 1;
+// while(num<=10){           //check before statement
+//     console.log(num++);
+// }                           
 
-switch(Number(status)){
-    case 1:
-        console.log("Morning");
-        break;
-    case 2:
-        console.log("Afternoon");
-        break;
-    case 3:
-        console.log("Night");
-        break;
-    default:
-        console.log("Error Number");
-        break;
-}
-console.log("Have a nice day");
+do{ 
+    console.log(num++);
+}while(num<=10);  //check after statement
+
+console.log("End");
+
